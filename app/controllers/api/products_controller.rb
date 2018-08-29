b73a8b9fc96e33e1abeb29609e8c914bd227b4e3
byrne-saw <byrnes.aw@gmail.com>
2018-08-28 19:24:51 -0500

    adding routes to all and first

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,13 @@
 class Api::ProductsController < ApplicationController
+
+  def all_products
+    @products = Product.all
+    render 'all_products.json.jbuilder'
+  end
+
+  def first_product
+    @first_product = Product.first
+    render 'first_product.json.jbuilder'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  # STEP 1: A ROUTE triggers a controller action
-  # verb "/urls" => "namespace/controllers#action"
+  namespace :api do
+    get '/all_products' => 'products#all_products'
+    get '/first_product' => 'products#first_product'
+  end
 end
