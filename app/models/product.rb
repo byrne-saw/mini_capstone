7887efa086694016908a7ff998c6f82d01c02112
byrne-saw <byrnes.aw@gmail.com>
2018-09-04 21:33:58 -0500

    day 13 updates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :description, presence: true
+  # validates :description, length: { in: 20..200 }
+  validates :description, length: { minimum: 20 }
+  validates :description, length: { maximum: 200 }
+
+
+  validates :image_url, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  # validates :price, numericality: true # ,redundant becasue of the line above 
 
   def is_discounted?
     price < 4
