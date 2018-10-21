40ce05973ae5f69d0b4a7d329f44892089bb7e0e
byrne-saw <byrnes.aw@gmail.com>
2018-10-21 16:37:43 -0500

    adding more values to cookie

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -10,7 +10,8 @@ class Api::SessionsController < ApplicationController
          Rails.application.credentials.fetch(:secret_key_base), # the secret key
          'HS256' # the encryption algorithm
       )
-      render json: {jwt: jwt, email: user.email, user_id: user.id}, status: :created
+      
+      render json: {jwt: jwt, email: user.email, user_id: user.id}, status: :created # personalize session - update key/values
     else
       render json: {}, status: :unauthorized
     end
